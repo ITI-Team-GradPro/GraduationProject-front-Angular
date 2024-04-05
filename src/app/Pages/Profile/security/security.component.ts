63cecc87c726fb37735883d3e0e9ff9866c96e55
Tasneem <Tasneem@TASNEEM>
2024-04-05 12:31:34 +0200

    finsh change password profile

--- src/app/Pages/Profile/security/security.component.ts
+++ src/app/Pages/Profile/security/security.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ChangePasswordService } from '../../../Services/ChangePassword/change-password.service';
+import { AuthService } from '../../../Services/Auth/auth.service';
+import { LoginService } from '../../../Services/Login/login.service';
 
 @Component({
   selector: 'app-security',
   templateUrl: './security.component.html',
-  styleUrl: './security.component.css'
+  styleUrls: ['./security.component.css']
 })
-export class SecurityComponent {
+export class SecurityComponent implements OnInit {
+  userData: any;
+  passwordData:object= {};
+    constructor(private authService: AuthService, private passwordService: ChangePasswordService , private _LoginServices : LoginService) { 
 
-}
+    }
+    ngOnInit(): void {
+      this._LoginServices.user();
+      this.userData = this._LoginServices.UserData;
+    }
+
+    
+    changePassword(userId: string) {
+      let oldPassword =(document.getElementById('oldPassword') as HTMLInputElement).value;
+      let newPassword =(document.getElementById('newPassword') as HTMLInputElement).value;
+     this.passwordData= {
+      oldPassword : oldPassword ,
+      newPassword : newPassword,
+      userId :userId
+    };
+
+    console.log(this.passwordData);
+      this.passwordService.changePassword(this.passwordData)
+        .subscribe({
+          next: (response) => {
+            console.log(response);
+              alert('Password changed successfully');
+          },
+          error: (error) => {
+            console.error('Error:', error);
+            alert('Failed to change password: ' + error.message);
+          }
+        });
+    }
+  }
+  
+
+
--- src/app/Services/Auth/auth.service.ts
+++ src/app/Services/Auth/auth.service.ts
@@ -7,6 +7,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthService {
+  [x: string]: any;
+  private apiUrl = 'https://localhost:44329/api/GetUserWithRole/'; 
 
   constructor(private _HttpClient: HttpClient) { }
 
@@ -18,5 +20,12 @@ export class AuthService {
   HostSignUp(userData:object):Observable<any>
   {
     return this._HttpClient.post('http://localhost:5004/api/Register/Host', userData);
+
+
+  }
+
+  getUserInfo(UserRoleName: string): Observable<any> {
+    const url = `${this.apiUrl}${UserRoleName}`; 
+    return this._HttpClient.get<any>(url);
   }
 }
